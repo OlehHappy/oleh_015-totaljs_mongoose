375048046992aa4ae451a1bd0ab193004a67207f
Oleh Daybov <olegtranslator@gmail.com>
2016-02-20 22:23:34 +0200

    all is working

--- models/user.js
+++ models/user.js
@@ -2,3 +2,19 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({ alias: String, created: Date })
 exports.schema = mongoose.model('user', userSchema);
 exports.name = 'user';
+
+// compile our model
+// var Person = mongoose.model('User', userSchema);
+
+// create a document
+// var record = new Person();
+//
+// record.alias = 'test Alias';
+// record.created = new Date();
+//
+// record.save(function() {
+//   User.find().all(function(arr) {
+//      console.log(arr);
+//      console.log('length='+arr.length);
+//    });
+// });
